ea559d11f6eaf2407d08056b803bf70a361f7497
马克喵MACat <39859514+geeks1230@users.noreply.github.com>
2026-04-10 05:05:55 +0800

    fix: 移除 distDir，使用默认 .next 输出

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 /* eslint-disable @typescript-eslint/no-var-requires */
 const nextConfig = {
-  distDir: '.vercel/output/static',
 
   eslint: {
     dirs: ['src'],
